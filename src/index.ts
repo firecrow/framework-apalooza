28957d88a9877b15c6a5b1df922e62c7d7b67621
Firecrow Silvernight <stephen@firecrow.com>
2024-05-15 17:48:24 -0500

    adding basic svelte class, not yet included in build

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,49 @@ import {Layout} from "./ui-react/layout";
 import * as React from "react";
 
 export class ReactApp  {
-     constructor(){
+    container: HTMLElement ;
+    constructor(){
         let x:number = 0;
         console.log(x + 100);
 
-        const container = document.getElementById("react-root");
-        const root = createRoot(container);
+        this.container = document.getElementById("react-root");
+        const root = createRoot(this.container);
         root.render(React.createElement(Layout, {}));
+    }
+    hide(){
+        this.container.classList.add('hidden');
+    }
+    show(){
+        this.container.classList.remove('hidden');
+    }
+}
+
+export class VueApp {
+    constructor(){
+        console.log('Yay Vue');
+    }
+    hide(){
+    }
+    show(){
+    }
+}
+
+export class SvelteApp {
+    constructor(){
+        console.log('Yay Svelte');
+    }
+    hide(){
+    }
+    show(){
+    }
+}
+
+export class AngularApp {
+    constructor(){
+        console.log('Yay Angular');
+    }
+    hide(){
+    }
+    show(){
     }
 }
