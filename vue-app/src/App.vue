cbd22bb1fe48b6220e2b41f1327dfdcf15274954
Firecrow Silvernight <stephen@firecrow.com>
2024-05-29 12:00:51 -0500

    blocking in vue app

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+    let data = {
+        headers: ['', '', 'Product', 'Qrtr0', 'Qrtr1','Qrtr2','Qrtr3','Qrtr4','Qrtr5','Qrtr6','Qrtr7'],
+        rows: [
+            ['Basic', 'Growth',0,0,0,0,0,0,0,0],
+            ['Basic', 'Units',0,0,0,0,0,0,0,0],
+            ['Basic', 'Price',0,0,0,0,0,0,0,0],
+            ['Basic', 'Total',0,0,0,0,0,0,0,0],
+            ['Advanced', 'Growth',0,0,0,0,0,0,0,0],
+            ['Advanced', 'Units',0,0,0,0,0,0,0,0],
+            ['Advanced', 'Price',0,0,0,0,0,0,0,0],
+            ['Advanced', 'Total',0,0,0,0,0,0,0,0],
+            ['Pro', 'Growth',0,0,0,0,0,0,0,0],
+            ['Pro', 'Units',0,0,0,0,0,0,0,0],
+            ['Pro', 'Price',0,0,0,0,0,0,0,0],
+            ['Pro', 'Total',0,0,0,0,0,0,0,0],
+        ]
+    }
 </script>
 
 <template>
